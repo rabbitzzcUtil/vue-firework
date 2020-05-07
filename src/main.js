66cb2493390ae3234d904c9338b39d1ed5f46a26
zhouzhechao <zzclovelcs@gmai.com>
2020-05-07 14:19:19 +0800

    v0.3.0

--- src/main.js
+++ src/main.js
@@ -1,22 +1,5 @@
 import VueFirework from './firework.vue';
 
+VueFirework.install = (Vue) => Vue.component('VueFirework', VueFirework);
 
-// 存储组件列表
-const components = [
-  VueFirework,
-];
-
-const install = (Vue) => {
-  if (install.installed) return;
-  components.map((component) => Vue.component(component.name, component));
-};
-
-// 判断是否是直接引入文件
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
-}
-
-export default {
-  install,
-  VueFirework,
-};
+export default VueFirework;
